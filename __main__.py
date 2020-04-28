3861c0ac0cdbcdd07f0a52c85f7e8a970fa33194
Dzhabrail <elmezov007@gmail.com>
2020-04-28 16:01:46 +0300

    Start button

--- __main__.py
+++ __main__.py
@@ -19,14 +19,14 @@ web = 'https://rif-rostov.ru/price/?arCrops%5B%5D=127'
 def start_message(message):
 
     markup = telebot.types.InlineKeyboardMarkup()
-    markup.row(telebot.types.InlineKeyboardButton(text='Ячмень', callback_data=3),
-               (telebot.types.InlineKeyboardButton(text='Пшеница', callback_data=4)))
+    markup.row(telebot.types.InlineKeyboardButton(text='Ячмень', callback_data='Ячмень'),
+               (telebot.types.InlineKeyboardButton(text='Пшеница', callback_data='Пшеница')))
 
-    markup.row(telebot.types.InlineKeyboardButton(text='Семечка', callback_data=5),
-               (telebot.types.InlineKeyboardButton(text='Горох', callback_data=6)))
+    markup.row(telebot.types.InlineKeyboardButton(text='Семечка', callback_data='Семечка'),
+               (telebot.types.InlineKeyboardButton(text='Горох', callback_data='Горох')))
 
     markup.add(telebot.types.InlineKeyboardButton(
-        text='Кукуруза', callback_data=7))
+        text='Кукуруза', callback_data='Кукуруза'))
 
     if message.chat.type == 'private':
         bot.send_message(message.chat.id, '<b>Добрый день сегодня цены на зерно следующие:</b> \n',
@@ -35,26 +35,29 @@ def start_message(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
+    start_button = telebot.types.ReplyKeyboardMarkup(True, True).row('/start')
+
     markup = telebot.types.InlineKeyboardMarkup()
     markup.row(telebot.types.InlineKeyboardButton(text='Max и Min', callback_data=10),
                (telebot.types.InlineKeyboardButton(text='График цен', callback_data=11)))
+
     bot.answer_callback_query(
         callback_query_id=call.id, text='Ожидайте идёт подготовка цен!')
-    if call.data == '3':
-        bot.send_message(call.message.chat.id, 'New message')
-    elif call.data == '4':
+    if call.data == 'Ячмень':
+        bot.send_message(call.message.chat.id, 'New message',
+                         reply_markup=start_button)
+    elif call.data == 'Пшеница':
         bot.send_message(call.message.chat.id,
-                         getDataByURL(web), parse_mode='HTML')
-    elif call.data == '5':
-        bot.send_message(call.message.chat.id, 'New message')
-    elif call.data == '6':
-        bot.send_message(call.message.chat.id, 'New message')
-    elif call.data == '7':
-        bot.send_message(call.message.chat.id, 'New message')
-    elif call.data == '10':
-        bot.send_message(call.message.chat.id, 'New message')
-    elif call.data == '11':
-        bot.send_message(call.message.chat.id, 'New message')
+                         getDataByURL(web), parse_mode='HTML', reply_markup=start_button)
+    elif call.data == 'Семечка':
+        bot.send_message(call.message.chat.id, 'New message',
+                         reply_markup=start_button)
+    elif call.data == 'Горох':
+        bot.send_message(call.message.chat.id, 'New message',
+                         reply_markup=start_button)
+    elif call.data == 'Кукуруза':
+        bot.send_message(call.message.chat.id, 'New message',
+                         reply_markup=start_button)
     bot.edit_message_reply_markup(
         call.message.chat.id, call.message.message_id)
 
