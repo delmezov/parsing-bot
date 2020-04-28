1b1512dc7b1b560a9eac813d7794aee50536c656
Denis <dmusatov98@gmail.com>
2020-04-28 14:55:11 +0300

    add func
    func dict to text

--- parse.py
+++ parse.py
@@ -10,16 +10,13 @@ def get_html(url):
     return r.text
 
 
-web = 'https://rif-rostov.ru/price/?arCrops%5B%5D=127'
-
-
 def getDataByURL(url):
     text = get_html(url)
     soup = BeautifulSoup(text, 'lxml')
 
     newDict = {}
 
-    for tags in soup.find_all("div", "main-price-elevator-item"):
+    for tags in soup.find_all("div","main-price-elevator-item"):
         if "Новороссийск" in tags.find("p").text:
             nameOrg = tags.find("h3").text.strip().replace(u'\xa0', u' ')
 
@@ -32,63 +29,32 @@ def getDataByURL(url):
             if "\"" in nameOrg:
                 nameOrg = nameOrg[:nameOrg.rfind("\"")+1]
             newDict[nameOrg] = []
-            for tag in (tags.tbody.find_all("div", "name")):
+            for tag in (tags.tbody.find_all("div","name")):
                 classes.append(tag.text.strip())
-            for tag in (tags.tbody.find_all("tr", {"class", "price-data"})):
+            for tag in (tags.tbody.find_all("tr",{"class","price-data"})):
                 prices.append(tag.td.text.strip())
-            for tag in (tags.tbody.find_all("div", "props")):
+            for tag in (tags.tbody.find_all("div","props")):
                 dopInfo.append(tag.text.strip())
             for i in range(len(classes)):
-                newDict[nameOrg].append([classes[i].replace(u'\xa0', u' '), dopInfo[i].replace(
-                    u'\xa0', u' '), prices[i].replace(u'\xa0', u' ')])
+                newDict[nameOrg].append([classes[i].replace(u'\xa0', u' '),dopInfo[i].replace(u'\xa0', u' '),prices[i].replace(u'\xa0', u' ')])
             if (newDict[nameOrg] == []):
                 newDict.pop(nameOrg, None)
-
+            
     return newDict
 
-
-print(getDataByURL('https://rif-rostov.ru/price/?arCrops%5B%5D=127'))
-
-
-'''
-        stats.append(tag.find('div', {'class': "title"}).text)
-    urls.append('https://www.agroinvestor.ru' + tag.find("a", href=True).get('href'))
-urls = urls[:len(urls)-4]
-
-for i in range(len(stats)):
-    stats[i] = stats[i].strip()
+#print(list(getDataByURL("https://rif-rostov.ru/price/?arCrops%5B%5D=127").keys())[0])
 
 
-for i in range(len(stats)):
-    endContent = {'stats' : stats[i],
-                  'url' : urls[i]}
-    csv_read(endContent)
 
-response.close()
-print(response.status_code)'
-text = get_html(web)
-soup = BeautifulSoup(text, 'lxml')
+def dataToString(dict):
+    data = []
+    temp_str = ""
+    for (keys,values) in dict.items():
+        for value in values:
+            temp_str = "<b>" + keys + " - " + value[0] + " - " + value[2] + "</b>" + "\n"
+            temp_str += value[1]
+            data.append(temp_str)
 
-response = requests.get(url=url, proxies=proxies)
-
-stats = []
-urls = []
-
-for tag in soup.find_all("div","item"):
-    if (tag.find('div', {'class': "title"}) != None):
-        stats.append(tag.find('div', {'class': "title"}).text)
-    urls.append('https://www.agroinvestor.ru' + tag.find("a", href=True).get('href'))
-urls = urls[:len(urls)-4]
-
-for i in range(len(stats)):
-    stats[i] = stats[i].strip()
-
-
-for i in range(len(stats)):
-    endContent = {'stats' : stats[i],
-                  'url' : urls[i]}
-    csv_read(endContent)
-
-response.close()
-print(response.status_code)
-'''
+    return data
+    
+    
