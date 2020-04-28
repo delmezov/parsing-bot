65f25679e1ffa87333e6f3753417e5f8704178fd
Igor <rtvcojigor@gmail.com>
2020-04-28 16:01:06 +0300

    urls_dict&start_button

--- __main__.py
+++ __main__.py
@@ -12,15 +12,12 @@ now = datetime.datetime.now()
 
 print("Connected!")
 
-web = 'https://rif-rostov.ru/price/?arCrops%5B%5D=127'
-
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-
     markup = telebot.types.InlineKeyboardMarkup()
-    markup.row(telebot.types.InlineKeyboardButton(text='Ячмень', callback_data=3),
-               (telebot.types.InlineKeyboardButton(text='Пшеница', callback_data=4)))
+    markup.row(telebot.types.InlineKeyboardButton(text='Ячмень', callback_data='Ячмень'),
+               (telebot.types.InlineKeyboardButton(text='Пшеница', callback_data='Пшеница')))
 
     markup.row(telebot.types.InlineKeyboardButton(text='Семечка', callback_data=5),
                (telebot.types.InlineKeyboardButton(text='Горох', callback_data=6)))
@@ -35,16 +32,18 @@ def start_message(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
+    start = telebot.types.ReplyKeyboardMarkup(True, True).row('/start')
     markup = telebot.types.InlineKeyboardMarkup()
     markup.row(telebot.types.InlineKeyboardButton(text='Max и Min', callback_data=10),
                (telebot.types.InlineKeyboardButton(text='График цен', callback_data=11)))
     bot.answer_callback_query(
         callback_query_id=call.id, text='Ожидайте идёт подготовка цен!')
-    if call.data == '3':
-        bot.send_message(call.message.chat.id, 'New message')
-    elif call.data == '4':
+    if call.data == 'Ячмень':
         bot.send_message(call.message.chat.id,
-                         getDataByURL(web), parse_mode='HTML')
+                         getDataByURL(config.filter_params_dict[call.data]), parse_mode='HTML', reply_markup=start)
+    elif call.data == 'Пшеница':
+        bot.send_message(call.message.chat.id,
+                         getDataByURL(config.filter_params_dict[call.data]), parse_mode='HTML', reply_markup=start)
     elif call.data == '5':
         bot.send_message(call.message.chat.id, 'New message')
     elif call.data == '6':
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 #token = '1179027465:AAGJwtW8Aac4oRKP_NWry72wRmv4BUGbstc'
-token = "1179027465:AAGJwtW8Aac4oRKP_NWry72wRmv4BUGbstc"
+token = "1133608327:AAG8QQUqBuoGke4SM10o_3NYG3bl9SjP850"
 
 proxy_const = {'https': 'socks5://Dzhabri:a3e8af21@195.123.248.48:1490'}
+
+filter_params_dict = {'Пшеница' : 'https://rif-rostov.ru/price/?arCrops%5B%5D=127',
+                      'Ячмень' : 'https://rif-rostov.ru/price/?arCrops%5B%5D=128'}
